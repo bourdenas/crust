97e35138577d88882880ceb027c4d94389ad9483
bourdenas <bourdenas@gmail.com>
2022-05-18 21:24:43 +0200

    Move PerformerBase inside performer module.

--- src/animation/animator.rs
+++ src/animation/animator.rs
@@ -1,5 +1,7 @@
-use super::{Animated, FrameRangePerformer, Performer, TimerPerformer, TranslationPerformer};
-use crate::{components::AnimationRunningState, trust::Animation};
+use super::{
+    performer::PerformerBase, Animated, FrameRangePerformer, TimerPerformer, TranslationPerformer,
+};
+use crate::trust::Animation;
 use std::time::Duration;
 
 #[derive(Default)]
@@ -106,60 +108,5 @@ impl Animator {
         }
 
         time_consumed
-    }
-}
-
-#[derive(Default)]
-struct PerformerBase<P: Performer> {
-    performer: P,
-
-    wait_time: Duration,
-    state: AnimationRunningState,
-}
-
-impl<P> PerformerBase<P>
-where
-    P: Performer,
-{
-    fn new(performer: P) -> Self {
-        PerformerBase {
-            performer,
-            wait_time: Duration::ZERO,
-            state: AnimationRunningState::Init,
-        }
-    }
-
-    fn finished(&self) -> bool {
-        self.state == AnimationRunningState::Finished
-    }
-
-    fn start(&mut self, animated: &mut Animated, animation: &Animation, speed: f64) {
-        self.performer.start(animated, animation, speed);
-        self.state = AnimationRunningState::Running;
-    }
-
-    fn progress(
-        &mut self,
-        time_since_last_frame: Duration,
-        animated: &mut Animated,
-        animation: &Animation,
-        animation_delay: Duration,
-    ) -> Duration {
-        if animation_delay == Duration::ZERO {
-            self.performer.execute(animated, animation);
-            self.state = AnimationRunningState::Finished;
-            return Duration::ZERO;
-        }
-
-        self.wait_time += time_since_last_frame;
-        while animation_delay <= self.wait_time {
-            self.wait_time -= animation_delay;
-            if let AnimationRunningState::Finished = self.performer.execute(animated, animation) {
-                self.state = AnimationRunningState::Finished;
-                return time_since_last_frame - self.wait_time;
-            }
-        }
-
-        time_since_last_frame
     }
 }
--- src/animation/performer.rs
+++ src/animation/performer.rs
@@ -1,5 +1,6 @@
 use super::Animated;
 use crate::{components::AnimationRunningState, trust::Animation};
+use std::time::Duration;
 
 pub trait Performer {
     fn start(&mut self, animated: &mut Animated, animation: &Animation, speed: f64);
@@ -8,4 +9,59 @@ pub trait Performer {
     fn resume(&mut self, animated: &mut Animated);
 
     fn execute(&mut self, animated: &mut Animated, animation: &Animation) -> AnimationRunningState;
+}
+
+#[derive(Default)]
+pub struct PerformerBase<P: Performer> {
+    performer: P,
+
+    wait_time: Duration,
+    state: AnimationRunningState,
+}
+
+impl<P> PerformerBase<P>
+where
+    P: Performer,
+{
+    pub fn new(performer: P) -> Self {
+        PerformerBase {
+            performer,
+            wait_time: Duration::ZERO,
+            state: AnimationRunningState::Init,
+        }
+    }
+
+    pub fn finished(&self) -> bool {
+        self.state == AnimationRunningState::Finished
+    }
+
+    pub fn start(&mut self, animated: &mut Animated, animation: &Animation, speed: f64) {
+        self.performer.start(animated, animation, speed);
+        self.state = AnimationRunningState::Running;
+    }
+
+    pub fn progress(
+        &mut self,
+        time_since_last_frame: Duration,
+        animated: &mut Animated,
+        animation: &Animation,
+        animation_delay: Duration,
+    ) -> Duration {
+        if animation_delay == Duration::ZERO {
+            self.performer.execute(animated, animation);
+            self.state = AnimationRunningState::Finished;
+            return Duration::ZERO;
+        }
+
+        self.wait_time += time_since_last_frame;
+        while animation_delay <= self.wait_time {
+            self.wait_time -= animation_delay;
+            if let AnimationRunningState::Finished = self.performer.execute(animated, animation) {
+                self.state = AnimationRunningState::Finished;
+                return time_since_last_frame - self.wait_time;
+            }
+        }
+
+        time_since_last_frame
+    }
 }
