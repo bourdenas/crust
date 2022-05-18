b7a2c67b914a6438291bcdd208212e85073ab90b
bourdenas <bourdenas@gmail.com>
2022-05-18 22:32:55 +0200

    Added unit tests for FrameRangePerformer.

--- src/animation/frame_range.rs
+++ src/animation/frame_range.rs
@@ -112,3 +112,262 @@ fn set_frame(
         },
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        animation::{performer::PerformerBase, testing::Fixture, FrameRangePerformer, Performer},
+        components::AnimationRunningState,
+        trust::{Animation, FrameRangeAnimation},
+    };
+    use std::time::Duration;
+
+    #[test]
+    fn single_execution() {
+        let mut fixture = Fixture::new();
+
+        let animation = Animation {
+            frame_range: Some(FrameRangeAnimation {
+                start_frame: 2,
+                end_frame: 5,
+                delay: 100,
+                repeat: 1,
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        let mut performer = FrameRangePerformer::new();
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 2);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Running
+        );
+        assert_eq!(fixture.sprite.frame_index, 3);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Finished
+        );
+        assert_eq!(fixture.sprite.frame_index, 4);
+
+        // Test Performer using PerformerBase.
+        let mut fixture = Fixture::new();
+        let mut performer = PerformerBase::new(FrameRangePerformer::new());
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 2);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(50),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::from_millis(50)
+        );
+        assert_eq!(fixture.sprite.frame_index, 2);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(180),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::from_millis(150)
+        );
+        assert_eq!(fixture.sprite.frame_index, 4);
+        assert_eq!(performer.finished(), true);
+    }
+
+    #[test]
+    fn repeated_execution() {
+        let mut fixture = Fixture::new();
+
+        let animation = Animation {
+            frame_range: Some(FrameRangeAnimation {
+                start_frame: 3,
+                end_frame: 5,
+                delay: 100,
+                repeat: 2,
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        let mut performer = FrameRangePerformer::new();
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 3);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Running
+        );
+        assert_eq!(fixture.sprite.frame_index, 4);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Running
+        );
+        assert_eq!(fixture.sprite.frame_index, 3);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Finished
+        );
+        assert_eq!(fixture.sprite.frame_index, 4);
+
+        // Test Performer using PerformerBase.
+        let mut fixture = Fixture::new();
+        let mut performer = PerformerBase::new(FrameRangePerformer::new());
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 3);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(800),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::from_millis(300)
+        );
+        assert_eq!(fixture.sprite.frame_index, 4);
+        assert_eq!(performer.finished(), true);
+    }
+
+    #[test]
+    fn indefinite_execution() {
+        let mut fixture = Fixture::new();
+
+        let animation = Animation {
+            frame_range: Some(FrameRangeAnimation {
+                start_frame: 0,
+                end_frame: 5,
+                delay: 200,
+                repeat: 0,
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        let mut performer = FrameRangePerformer::new();
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 0);
+
+        for i in 1..100 {
+            let mut animated = fixture.animated();
+            assert_eq!(
+                performer.execute(&mut animated, &animation),
+                AnimationRunningState::Running
+            );
+            assert_eq!(fixture.sprite.frame_index, i % 5);
+        }
+
+        // Test Performer using PerformerBase.
+        let mut fixture = Fixture::new();
+        let mut performer = PerformerBase::new(FrameRangePerformer::new());
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 0);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(200),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::from_millis(200)
+        );
+        assert_eq!(fixture.sprite.frame_index, 1);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(1500),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::from_millis(1500)
+        );
+        assert_eq!(fixture.sprite.frame_index, 3);
+        assert_eq!(performer.finished(), false);
+    }
+
+    #[test]
+    fn zero_delay() {
+        let mut fixture = Fixture::new();
+
+        let animation = Animation {
+            frame_range: Some(FrameRangeAnimation {
+                start_frame: 1,
+                end_frame: 5,
+                delay: 0,
+                repeat: 0,
+                ..Default::default()
+            }),
+            ..Default::default()
+        };
+
+        let mut performer = FrameRangePerformer::new();
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 1);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.execute(&mut animated, &animation),
+            AnimationRunningState::Running
+        );
+        assert_eq!(fixture.sprite.frame_index, 2);
+
+        // Test Performer using PerformerBase.
+        //
+        // This is a corner case that makes little sense and doesn't have a
+        // clear correct behaviour. Implemented behaviour is that animation will
+        // apply only one frame change and finish after that.
+        let mut fixture = Fixture::new();
+        let mut performer = PerformerBase::new(FrameRangePerformer::new());
+        let mut animated = fixture.animated();
+        performer.start(&mut animated, &animation, 1.0);
+        assert_eq!(fixture.sprite.frame_index, 1);
+        assert_eq!(performer.finished(), false);
+
+        let mut animated = fixture.animated();
+        assert_eq!(
+            performer.progress(
+                Duration::from_millis(200),
+                &mut animated,
+                &animation,
+                Duration::from_millis(animation.frame_range.as_ref().unwrap().delay as u64)
+            ),
+            Duration::ZERO
+        );
+        assert_eq!(fixture.sprite.frame_index, 2);
+        assert_eq!(performer.finished(), true);
+    }
+}
