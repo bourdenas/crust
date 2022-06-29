9b896f6948496564f82d71ac146397ccf44224d5
bourdenas <bourdenas@gmail.com>
2022-06-29 16:59:02 +0200

    Add TileMap resource manager.

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -2,8 +2,10 @@ mod manager;
 mod manager_annotation;
 mod sprite_sheets;
 mod texture;
+mod tiles;
 
 pub use manager::{ResourceLoader, ResourceManager};
 pub use manager_annotation::{ResourceLoaderWithAnnotation, ResourceManagerWithAnnotation};
 pub use sprite_sheets::{SpriteSheet, SpriteSheetsManager};
 pub use texture::TextureManager;
+pub use tiles::*;
