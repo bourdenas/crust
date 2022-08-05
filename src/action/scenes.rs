22d8a0f99da43897a15945e3b069c5294cd5260f
bourdenas <bourdenas@gmail.com>
2022-08-05 11:15:13 +0200

    LoadScene action updates the World resource Viewport.

--- src/action/scenes.rs
+++ src/action/scenes.rs
@@ -1,25 +1,30 @@
-use crate::{crust::SceneAction, scene::SceneManager};
+use crate::{
+    crust::SceneAction,
+    resources::{Viewport, WindowSize},
+    scene::SceneManager,
+};
 use sdl2::rect::Rect;
-use specs::World;
+use specs::{World, WorldExt};
 
 pub struct Scenes;
 
 impl Scenes {
     pub fn load(scene_action: SceneAction, scene_manager: &mut SceneManager, world: &mut World) {
-        if let Err(e) = scene_manager.load(
-            &scene_action.resource,
-            match &scene_action.viewport {
-                Some(viewport) => Some(Rect::new(
+        if let Err(e) = scene_manager.load(&scene_action.resource, None, world) {
+            eprintln!("🦀 Failed to load scene: {:?}\nError: {e}", &scene_action);
+            return;
+        }
+
+        match &scene_action.viewport {
+            Some(viewport) => {
+                *world.write_resource() = Viewport(Rect::new(
                     viewport.left,
                     viewport.top,
                     viewport.width,
                     viewport.height,
-                )),
-                None => None,
-            },
-            world,
-        ) {
-            eprintln!("🦀 Failed to load scene: {:?}\nError: {e}", &scene_action);
+                ))
+            }
+            None => *world.write_resource() = Viewport(world.read_resource::<WindowSize>().0),
         }
     }
 }
